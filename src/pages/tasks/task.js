29c70d90fa2502f0968e1902487c7da35ed4ae52
SergeyDedikov <serded59@yandex.ru>
2022-06-05 17:35:51 +0500

    feat: link for main page

--- src/pages/tasks/task.js
+++ src/pages/tasks/task.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 export default function Task() {
   return (
     <section>
       <h1>Заголовок</h1>
       <p>Описание</p>
       <data>Дата создания</data>
+      <nav>
+        <Link href="/">
+          <a>Вернуться на главную</a>
+        </Link>
+      </nav>
     </section>
   );
 }
